97fcc43bc4d13d1c32b828978847d8fec039a87c
enricoderasmo21 <derasmo.enrico@gmail.com>
2023-03-30 12:47:07 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<script>
+import {store} from '../store.js';
+
+export default {
+  data() {
+    return {
+
+      store,
+    }
+  }
+}
+</script>
+
+<template>
+ 
+</template>
+
+<style scoped>
+
+</style>
